d77c95da0b7a674469b7a34e38a860eb34dcc0f8
Saqreed <saqreed80@gmail.com>
2026-08-27 19:06:50 +0400

    add empty login page

--- diploma-project/frontend/src/app/router.tsx
+++ diploma-project/frontend/src/app/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router'
 
 import { App } from '../App'
+import { LoginPage } from '../pages/LoginPage'
 
 export const applicationRouter = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+    ],
   },
 ])
---
+++ diploma-project/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,3 @@
+export function LoginPage() {
+  return <section id="login-page" aria-label="Login" />
+}
